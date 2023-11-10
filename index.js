28c45b7ad154f88b85c369551f575b26089e894b
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2023-11-10 17:57:51 -0500

    repo: misc update

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const createWorkspace = async () => {
 
   await appendJson(`${name}/package.json`, {
     scripts: {
-      do: "pnpm run --parallel --recursive --if-present",
+      dev: "pnpm run --parallel --recursive --if-present",
     },
   });
 
