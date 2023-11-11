49b0d15438455a61bb9eb53dd6760c2394b79070
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2023-11-11 17:04:37 -0500

    save work

--- resources/web/tailwind/app/layout.tsx
+++ resources/web/tailwind/app/layout.tsx
@@ -6,6 +6,29 @@ import Theme, { ThemeToggle } from "./(theme)";
 export const metadata: Metadata = {
   title: "My Next.js App",
   description: "Created a Next.js with Tailwind CSS and TypeScript",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/icons/favicon.ico",
+    shortcut: "/icons/favicon-1024.png",
+    apple: "/icons/favicon-1024.png",
+  },
   manifest: "/manifest.webmanifest",
   keywords: ["Next.js", "Tailwind CSS", "TypeScript"],
 };
