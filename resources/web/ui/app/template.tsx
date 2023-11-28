da3ec6d7ed7e696dcf253a455380422dff0d3ea0
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2023-11-27 19:59:28 -0500

    save work

--- resources/web/ui/app/template.tsx
+++ resources/web/ui/app/template.tsx
@@ -3,7 +3,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const variants = {
   enter: { opacity: 0, y: -15 },
-  exit: { opacity: 1, y: 0 },
+  center: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 15 },
 };
 
 export default function Template({ children }: { children: React.ReactNode }) {
@@ -12,7 +13,8 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <motion.div
         variants={variants}
         initial="enter"
-        animate="exit"
+        animate="center"
+        exit="exit"
         transition={{ type: "spring", damping: 20, stiffness: 100 }}
       >
         {children}
