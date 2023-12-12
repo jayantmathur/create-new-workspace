a5ab0bc5fd3a5da56ad719a5aca5a43dd50eee25
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2023-12-12 18:15:34 -0500

    save work

--- resources/web/mdx/app/content/[slug]/layout.tsx
+++ resources/web/mdx/app/content/[slug]/layout.tsx
@@ -3,7 +3,7 @@ import BackButton from "../components/backbutton";
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <BackButton className="fixed top-20 left-2 z-50" /> */}
+      <BackButton className="fixed z-50 sm:bottom-4 sm:left-4 sm:translate-x-0 bottom-14 left-1/2 -translate-x-1/2" />
       <div className="grid place-items-center">
         <div className="prose dark:prose-invert">{children}</div>
       </div>
