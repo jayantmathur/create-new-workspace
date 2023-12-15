c7e59b3e10b07c1b7189f52f9ba50638af0bfaee
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2023-12-15 00:14:56 -0500

    save work

---
+++ resources/utils/Rutils/myrutils/R/get_contrasts.R
@@ -0,0 +1,47 @@
+#' Get Contrasts
+#'
+#' This function is used to get contrasts from a given model and vector.
+#'
+#' @param model A model object. Provide lmer models with interactions.
+#' @param vector A vector. Provide a vector of the form ~ A | B.
+#'
+#' @return A data frame with the contrasts.
+#'
+#' @importFrom emmeans emmeans contrast
+#' @importFrom dplyr mutate across slice
+#'
+#' @examples
+#' \dontrun{
+#' score_model <- lmer(
+#'     Score ~ ConditionC + StageC + ConditionC:StageC + (1 | PID),
+#'     data = data,
+#'     control = control
+#' )
+#'
+#' get_contrasts(score_model, ~ StageC | ConditionC)
+#' }
+#'
+#' @export
+
+get_contrasts <- function(model, vector) {
+    output <- as.data.frame(
+        emmeans::contrast(
+            emmeans::emmeans(model, vector)
+        )
+    )
+
+    output <- dplyr::slice(
+        output,
+        2, 4
+    )[, -c(1, 4, 5)]
+
+    output <- dplyr::mutate(
+        output,
+        dplyr::across(
+            .cols = -c(1),
+            .fns = ~ round(.x, 3)
+        )
+    )
+
+    return(output)
+}
