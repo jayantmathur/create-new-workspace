d5d8e59d809811810e90b3a0333a3816b8231039
jayantmathur <32935713+jayantmathur@users.noreply.github.com>
2024-06-24 11:35:23 -0500

    quick save

--- resources/R/setup.R
+++ resources/R/setup.R
@@ -1,4 +1,4 @@
-min_packages <- c("IRkernel", "tidyverse", "ggpubr", "devtools", "emmeans")
+min_packages <- c("IRkernel", "tidyverse", "ggpubr", "devtools", "emmeans","stringi")
 install.packages(
     min_packages,
     repos = "https://cloud.r-project.org"
